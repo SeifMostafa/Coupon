fdbb1680f64de997e1fd6d2a035da2bec4316aa6
csseifms <csseifms@gmail.com>
2021-12-14 15:03:15 +0200

    issue while auth - 401

--- src/main/java/com/musala/couponservice/security/ResourceServerConfig.java
+++ src/main/java/com/musala/couponservice/security/ResourceServerConfig.java
@@ -1,27 +1,51 @@
 package com.musala.couponservice.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
+
 @Configuration
-@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String RESOURCE_ID = "couponservice";
+    @Value("${publicKey}")
+    private String publicKey;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(AuthorizationServerConfig.RESOURCE_ID);
+        resources.resourceId(RESOURCE_ID);
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().
-                mvcMatchers(HttpMethod.GET, "/couponapi/coupons/**").
+                mvcMatchers(HttpMethod.GET, "/couponapi/coupons/{code:^[A-Z]*$}").
                 hasAnyRole("USER", "ADMIN").mvcMatchers(HttpMethod.POST, "/couponapi/coupons")
                 .hasAnyRole("ADMIN").
                 anyRequest().denyAll().and().csrf().disable();
 
+    }
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setVerifierKey(publicKey);
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new JwtTokenStore(jwtAccessTokenConverter());
     }
 }
